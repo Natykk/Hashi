737a5335d7a1397aeea33a266f90855dd4618aff
raphael <Raphael.Abart.Etu@univ-lemans.fr>
2024-01-30 11:02:32 +0100

    précision des attributs hérités et setGrille

--- src/com/hashi/grille/Ile.java
+++ src/com/hashi/grille/Ile.java
@@ -2,17 +2,20 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Ile extends Case{
-    // int taille; (px)
+    // attributs hérités
+        // int x, y; // coordonnées de la position dans la grille
+        // static Grille grille; // référence vers la grille où cette île se situe
     private int valeur;
+    
     private List<Pont> listePont;
 
 
     protected Ile(int valeur,int x,int y,Grille lagrille) {
         super(x,y);
+        setGrille(lagrille);
+
         this.valeur = valeur;
         this.listePont = new ArrayList<>();
-
-        grille = lagrille;
     }
 
     public Case getPosition() {
