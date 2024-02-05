d10ba7461f021b2ab06526aaacf93613a06f0810
Rabia Allagui <allaguirabia77@gmail.com>
2024-02-05 11:26:55 +0100

    ajout gestion menu

---
+++ src/com/Gestion de menus/PageManager.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class PageManager {
+
+    public static void changerPage(JFrame frame, JPanel nouvellePage) {
+        frame.getContentPane().removeAll();
+        frame.getContentPane().add(nouvellePage);
+        frame.revalidate();
+        frame.repaint();
+    }
+
+    public static void afficherMessage(JFrame frame, String message) {
+        JOptionPane.showMessageDialog(frame, message);
+    }
+}
