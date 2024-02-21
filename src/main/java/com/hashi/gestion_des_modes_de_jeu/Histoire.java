3aee5428c885b4efdf34cb397b4ef8a9e99668d0
Medos69 <medbesbes2014@gmail.com>
2024-02-21 14:37:25 +0100

    Combien de ponts reste a mon voisin ?

---
+++ src/main/java/com/hashi/gestion_des_modes_de_jeu/Histoire.java
@@ -0,0 +1,5 @@
+package main.java.com.hashi.gestion_des_modes_de_jeu;
+
+public class Histoire implements MDJ{
+    int temps;
+}
---
+++ src/main/java/com/hashi/gestion_des_modes_de_jeu/readme.md
@@ -0,0 +1,1 @@
+le fichier MDJ.java represente une interface pour les modes de jeu
