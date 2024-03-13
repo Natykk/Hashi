bd9973afecc8fd772d0c3b7f7bd479eabcfb4cb6
raphael <Raphael.Abart.Etu@univ-lemans.fr>
2024-03-13 10:49:22 +0100

    ajout espaces

--- src/main/java/com/hashi/grille/Aide.java
+++ src/main/java/com/hashi/grille/Aide.java
@@ -14,15 +14,20 @@ public enum Aide {
 	FORCE8, // une île de valeur 8 = compléter l'île avec 8 ponts
 
 	BLOQUE3, // une île de valeur 3 qui a 2 voisins, dont 1 libre et 1 pont avec un voisin qui est
-				// complété = on peut compléter l'île avec 2 ponts sur son autre voisin
+
+	// ci-dessous: pas encore appliqués
 	BLOQUE41, // une île de valeur 4 qui a 3 voisins, dont 2 libres et le 3e est complété 
 				//avec 1 pont lié à cette Ile = on peut ajouter 1 Pont pour chaque autre voisin
+
 	BLOQUE42, // une île de valeur 4 qui a 1 voisin libre et 1 pont avec 2 voisins qui sont
 				// complétés = on peut compléter l'île avec 2 ponts sur son 3e et dernier voisin
+
 	BLOQUE5, // une île de valeur 5 qui a 3 voisins libres et 1 pont avec un voisin qui est
 				// complété = on peut compléter l'île avec 2 ponts sur ses 2 autres voisins
+
 	BLOQUE6, // une île de valeur 6 qui a 4 voisins libres et 1 pont avec un voisin qui est
 				// complété = on peut ajouter 1 pont pour chaque voisin
+
 	BLOQUE7 // une île de valeur 7 qui a 1 pont avec un voisin qui est complété = on peut
 			// compléter l'île avec 2 ponts sur ses 3 autres voisins
 }
